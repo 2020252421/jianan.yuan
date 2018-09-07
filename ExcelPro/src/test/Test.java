b8f69cb4e9bfb8a2046b6a3db1723d35f24ada28
2020252421 <www.2020252421@qq.com>
2018-09-07 22:40:35 +0800

    excel

--- ExcelPro/src/test/Test.java
+++ ExcelPro/src/test/Test.java
@@ -6,8 +6,8 @@ import util.ExcelUtuil;
 
 public class Test {
 	public static void main(String[] args) {
-		String path = "D://Excel//";
-		String fileName = "513.xls";
+		String path = "C://Users//ASUS//Desktop//excel//";
+		String fileName = "513 .xls";
 		HSSFWorkbook workbook = ExcelUtuil.readExcel(path, fileName);
 		for (int i = 1; i < 17; i++) {
 			ExcelUtuil.getPoint(i, workbook, "ÁÑ·ìË®Æ½");
